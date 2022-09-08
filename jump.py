c204ffd91d8a7a4a524cb63d5855a3e1f0934ada
JJKilla24 <97300403+JJKilla24@users.noreply.github.com>
2022-09-08 19:31:42 +0000

    up

---
+++ jump.py
@@ -0,0 +1,22 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+   move()
+   move()
+  move()
+  turnleft()
+  move()
+  move()
+  move()
+move()
+turnleft()
+turnleft()
+turnleft()
+Move()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
