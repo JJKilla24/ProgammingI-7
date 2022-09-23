9d65a74d6487bfb5cc0a1f80238c833285a8ffde
JJKilla24 <97300403+JJKilla24@users.noreply.github.com>
2022-09-23 20:59:59 +0000

    update

---
+++ K26.py
@@ -0,0 +1,15 @@
+from stanfordkarel import *
+
+move()
+move()
+turn_left()
+move()
+move()
+def main():
+    """ Karel code goes here! """
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
