6a279a8ba38e5554622a39c98f55cb8c53d362b8
JJKilla24 <97300403+JJKilla24@users.noreply.github.com>
2022-09-26 00:38:25 +0000

    update

--- K26.py
+++ K26.py
@@ -1,13 +1,44 @@
 from stanfordkarel import *
 
-move()
-move()
-turn_left()
-move()
-move()
+
 def main():
     """ Karel code goes here! """
-    
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
     pass
 
 
