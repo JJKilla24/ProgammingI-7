5d88afbd9a39a3c9a87745617fe79d284c71095f
JJKilla24 <97300403+JJKilla24@users.noreply.github.com>
2022-09-27 19:34:24 +0000

    update

--- k515.py
+++ k515.py
@@ -1,158 +1,167 @@
-from stanfordkarel import *
-from time import sleep
+run_karel_program()from stanfordkarel import *
 
 
 class ktools:
- def m(self):
-  """Shorthand for Move"""
-  move()
+  def m(self):
+    """Shorthand for Move"""
+    move()
 
- def tl(self):
-  """Turn left"""
-  turn_left() 
+  def tl(self):
+    """Turn Left"""
+    turn_left()
 
- def tr(self):
-  """Turn Right"""
-  self.tl()
-  self.tl()
-  self.tl()
-
- def ta(self):
-    """Turn around"""
+  def tr(self):
+    """Turn Right"""
+    self.tl()
     self.tl()
     self.tl()
 
- def pick(self):
-      """Pickbeeper"""
-      put_beeper()
+  def ta(self):
+    """Turn Around"""
+    self.tl()
+    self.tl()
 
- def put2(self):
-      """Put 2 beepers in a line"""
-      self.put()
-      self.m()
-      self.put()
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
 
- def put5(self):
-      """Put 5 beepers in a line"""
-      self.put2()
-      self.m()
-      self.put2
-      self.m()
-      self.put()
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
 
+  def put2(self):
+    """Put 2 beepers in a line"""
+    self.put()
+    self.m()
+    self.put()
+
+  def put5(self):
+    """Put 5 beepers in a line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
 
   def h(self):
-    
-      def fic(self) -> bool:
-        """Front is clear"""
-      return front_is_clear()
+    """Print H using beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+
+  def fic(self) -> bool:
+    """Front is Clear"""
+    return front_is_clear()
 
   def fib(self) -> bool:
-      """Front is Blocked"""
-      return not self.fic()
+    """Front is Blocked"""
+    return not self.fic()
 
   def ric(self) -> bool:
-      """Right is Clear"""
-      self.tr()
-      if self.fic():
-        self.tl()
-        return True 
+    """Right is Clear"""
+    self.tr()
+    if self.fic():
       self.tl()
-      return False
+      return True  # Immediately exit the function
+    self.tl()
+    return False
 
   def rib(self) -> bool:
-     """right is block"""
+    """Right is Blocked"""
     return not self.ric()
 
-
   def mazemove(self):
-      """Maze Move"""
+    """Maze Move"""
     if self.fib():
-      self.tf()
-    else:
+      self.tl()
+    else:  # Otherwise...
       self.m()
-    if self.ric():
-      self.tr()
-      self.m()
-    if self.ric():
-      self.tr()
-      self.m()
+      if self.ric():
+        self.tr()
+        self.m()
+        if self.ric():
+          self.tr()
+          self.m()
     pass
 
   def mm(self, num):
-      """Move Multiple"""
-      for number in range(0, num):
-        self.m()
-
+    """Move Multiple"""
+    for number in range(0, num):
+      self.m()
 
   def putm(self, num):
     """Put Multiple"""
     for i in range(num - 1):
       self.put()
       self.m()
-      self.put()
-
+    self.put()
 
   def pickm(self, num):
-        """Pick Multiple"""
-        for _ in range(num - 1):
-          self.pick()
-          self.m()
-          self.pick()
-          
+    """Pick Multiple"""
+    for _ in range(num - 1):
+      self.pick()
+      self.m()
+    self.pick()
 
-  def SOB(self):
-        """Standing on Beeper"""
-        return beepers_present()
-        
+  def SOB(self) -> bool:
+    """Standing on Beeper"""
+    return beepers_present()
 
   def jump(self):
-           """Jump for 510"""
-           while self.fic():
-             self.m()
-           self.tl()
-           while self.rib():
-              self.m()
-           self.tr()
-           self.m()
-           self.tr()
-            while self.fic():
-              self.m()
-              self.tl()
+    """Jump for 510"""
+    while self.fic():
+      self.m()
+    self.tl()
+    while self.rib():
+      self.m()
+    self.tr()
+    self.m()
+    self.tr()
+    while self.fic():
+      self.m()
+    self.tl()
 
   def find(self):
-                """Find for 515"""
-                while not facing_north():
-                  self.tl()
-                  self.m()
-                  if not self.SOB():
-                    self.tl()
-                    self.m()
-                    self.tl()
-                    self.m()
-                  for _ in range(2):
-                    if not self.SOB():
-                      self.m()
-                      self.tl()
-                      self.m()
-                      
-                  pass
-      
+    """Find for 515"""
+    while not facing_north():
+      self.tl()
+    self.m()
+    if not self.SOB():
+      self.tl()
+      self.m()
+      self.tl()
+      self.m()
+    for _ in range(2):
+      if not self.SOB():
+        self.m()
+        self.tl()
+        self.m()
+    pass
+    
 
- def main():
+
+def main():
     """ Karel code goes here! """
-   kt = ktools()
-    kt.m()
-    kt.tl()
-    kt.m()
-    kt.tr()
-    kt.m() 
-    while kt.SOB():
-      kt.pick()
-      kt.find()
-    
-
-    
+    kt = ktools()
     
     pass
 
