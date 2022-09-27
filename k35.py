9d7afbe4e63c9b0a8e28519e5d53b6a03d91a934
JJKilla24 <97300403+JJKilla24@users.noreply.github.com>
2022-09-27 23:46:30 +0000

    update

--- k35.py
+++ k35.py
@@ -5,46 +5,49 @@ class ktools:
   def m(self):
     """Shorthand for Move"""
     move()
-
+    
   def tl(self):
     """Turn Left"""
     turn_left()
-
+    
   def tr(self):
-   """Turn Right"""
-  self.tl()
-  self.tl()
-  self.tl()
-
+    """Turn Right"""
+    self.tl()
+    self.tl()
+    self.tl()
+    
   def ta(self):
-    """Turn around"""
+    """Turn Around"""
     self.tl()
     self.tl()
-
-    def pick(self):
-      """Pickbeeper"""
-      put_beeper()
-
-      def put2(self):
-       """Put 2 beepers in a line"""
-      self.put()
-      self.m()
-      self.put()
-
-      def put5(self):
-       """Put 5 beepers in a line"""
-      self.put2()
-      self.m()
-      self.put2
-      self.m()
-      self.put()
-
-
-    def h(self):
-     """Print H using beepers"""
+    
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+    
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+    
+  def put2(self):
+    """Put 2 beepers in a line"""
+    self.put()
+    self.m()
+    self.put()
+    
+  def put5(self):
+    """Put 5 beepers in a line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+    
+  def h(self):
+    """Print H using beepers"""
     self.tl()
     self.put5()
-    self.tf()
+    self.tr()
     self.m()
     self.m()
     self.m()
@@ -65,16 +68,20 @@ class ktools:
     self.m()
     self.m()
 
-    
-
-
+    def e(self):
+      """Print E using beepers"""
+      self.tl()
+      self.put5()
+      
+      
+  
 def main():
     """ Karel code goes here! """
     kt = ktools()
     kt.h()
-    
+        
     pass
-
-
+  
+  
 if __name__ == "__main__":
     run_karel_program()
