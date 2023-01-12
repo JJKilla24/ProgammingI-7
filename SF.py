e075a8154dd96a4e3174186df4720dc86752a34c
JJKilla24 <97300403+JJKilla24@users.noreply.github.com>
2023-01-12 20:36:04 +0000

    u

--- SF.py
+++ SF.py
@@ -1,67 +1,84 @@
-import pygame
-pygame.init()
+import sys
+import random
+import pygame as pg 
+from time import sleep
 
-win = pygame.display.set_mode((500,500))
-pygame.display.set_caption("Placeholder")
 
-x = 50
-y = 50
-width = 30
-height = 30
-vel = 5
 
-x = 50
-y = 50
-width = 30
-height = 30
-vel = 5
+pg.init()
+screen = pg.display.set_mode((640, 480))
+pg.display.set_caption("Dodge or Die")
 
-run = True
+XWING_IMG = pg.Surface((25, 38))
+XWING_IMG.fill((90, 120, 150))
+TIE_IMG = pg.Surface((40, 24))
+TIE_IMG.fill((190, 60, 50))
+BG_COLOR = pg.Color('gray15')
 
-while run:
-    pygame.time.delay(100)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+def main():
+    clock = pg.time.Clock()
+    # Surfaces/images have a `get_rect` method which 
+    # returns a rect with the dimensions of the image.
+    player_rect = XWING_IMG.get_rect()
+    player_rect.center = (300, 400)
+    change_x = 0
+    change_y = 0
+    enemies = []
+    spawn_counter = 30
 
-    keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_LEFT]:
-        x -= vel
+    done = False
 
-    if keys[pygame.K_RIGHT]:
-        x += vel
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_d:
+                    change_x = 5
+                if event.key == pg.K_a:
+                    change_x = -5
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_d and change_x > 0:
+                    change_x = 0
+                if event.key == pg.K_a and change_x < 0:
+                    change_x = 0
 
-    if keys[pygame.K_UP]:
-        y -= vel
+        # Spawn enemies if counter <= 0 then reset it.
+        spawn_counter -= 1
+        if spawn_counter <= 0:
+            # Append an enemy rect. You can pass the position directly as an argument.
+            enemies.append(TIE_IMG.get_rect(topleft=(random.randrange(600), 0)))
+            spawn_counter = 30
 
-    if keys[pygame.K_DOWN]:
-        y += vel
-    pygame.time.delay(100)
+        # Update player_rect and enemies.
+        player_rect.x += change_x
+        player_rect.y += change_y
+        for enemy_rect in enemies:
+            enemy_rect.y += 5
+            # Collision detection with pygame.Rect.colliderect.
+            if player_rect.colliderect(enemy_rect):
+              hits = 0
+              hits += 1
+              if hits == 1:
+                print('Collision 1 two left')
+              elif hits == 2:
+                print('Collision 2 one left')
+              elif hits == 3:
+                print('Collision 3 Last one')
+              
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        # Draw everything.
+        screen.fill(BG_COLOR)
+        for enemy_rect in enemies:
+            screen.blit(TIE_IMG, enemy_rect)
+        screen.blit(XWING_IMG, player_rect)
 
-    keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_A]:
-        x -= vel
+        pg.display.flip()
+        clock.tick(30)
 
-    if keys[pygame.K_D]:
-        x += vel
-
-    if keys[pygame.W_]:
-        y -= vel
-
-    if keys[pygame.K_S]:
-        y += vel
-
-      
-    
-    win.fill((255,255,255))  # Fills the screen with black
-    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
-    pygame.display.update() 
-    
-pygame.quit()
+  
+if __name__ == '__main__':
+    main()
+    pg.quit()
+    sys.exit()
